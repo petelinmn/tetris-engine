677fd7728a6c8b97cb6e5076e57377bc29e61ee6
mn.petelin <mn.petelin@tensor.ru>
2018-09-04 13:45:25 +0500

    rendering gamefield

--- test/index.js
+++ test/index.js
@@ -1,22 +1,15 @@
-let shapeSet = require('../dist/shapes/tetra-shapes');
-let Shape = require('../dist/shape').Shape;
 
-let shape = new Shape(shapeSet);
+let GameField = require('../dist/gamefield');
 
-console.log(shape);
-shape.rotate();
-
-console.log(shape);
-shape.rotateBack();
-
+let game = new GameField(20, 40);
 
 let App = new Vue({
     template:
-        `<table class="figure-show-table">
+        `<table class="game-table">
             <tbody>
                 <tr v-for="row in shape.body">
                     <td v-for="cell in row"
-                        v-bind:class="{ figure: cell == 1, empty: cell.val == 0 }">
+                        v-bind:class="{ figure: cell.val == 1, empty: cell.val == 0 }">
                     </td>
                 </tr>
             </tbody>
@@ -25,12 +18,12 @@ let App = new Vue({
     data() {
         
         return {
-            shape: new Shape(shapeSet)
+            shape: new GameField(20, 30)
         }
     },
     methods: {
         render() {
-            this.shape =  new Shape(shapeSet);
+            this.shape =  new GameField(20, 30);
         }
     },
     beforeMount() {
@@ -39,3 +32,40 @@ let App = new Vue({
         }, 1000)
     }
 });
+
+
+//let shapeSet = require('../dist/shapes/tetra-shapes');
+//let Shape = require('../dist/shape').Shape;
+
+
+//let shape = new Shape(shapeSet);
+
+// let App = new Vue({
+//     template:
+//         `<table class="figure-show-table">
+//             <tbody>
+//                 <tr v-for="row in shape.body">
+//                     <td v-for="cell in row"
+//                         v-bind:class="{ figure: cell == 1, empty: cell.val == 0 }">
+//                     </td>
+//                 </tr>
+//             </tbody>
+//         </table>`,
+//     el: '#app',
+//     data() {
+        
+//         return {
+//             shape: new Shape(shapeSet)
+//         }
+//     },
+//     methods: {
+//         render() {
+//             this.shape =  new Shape(shapeSet);
+//         }
+//     },
+//     beforeMount() {
+//         setInterval(()=>{
+//             this.render()
+//         }, 1000)
+//     }
+// });
