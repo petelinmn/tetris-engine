f2474902deea3197ec93973cafa51e41935a708f
mn.petelin <mn.petelin@tensor.ru>
2018-09-04 13:15:01 +0500

    Added class GameField

--- dist/engine.js
+++ dist/engine.js
@@ -1,9 +1,17 @@
 /**
- * Implements the core of game
+ * Implements the engine of game
  */
 
 class Engine {
-   constructor() {
+
+    /**
+     * Initializing new Engine
+     * @param {*} renderHandle The method that will be runned every time when game state will be changed. 
+     * Receives game render data.
+     * @param {*} width is the width of the field of the game in squares
+     * @param {*} height is the height of the field of the game in squares
+     */
+   constructor(renderHandle, width, height) {
       console.log('Hello from tetris-engine! v 1.0.8!');
    }
 }
---
+++ dist/gamefield.js
@@ -0,0 +1,39 @@
+/**
+ * Implements the area of a game
+ */
+
+class GameField {
+
+    /**
+     * Initializing new area
+     * @param {*} width is the width of the field of the game in squares
+     * @param {*} height is the height of the field of the game in squares
+     */
+   constructor(width = 15, height = 20) {
+      if(width <= 0 || height <= 0)
+        throw 'Size parameters of GameField are incorrect'
+
+      this.width = width;
+      this.height = height;
+   }
+
+   get body() {
+    let body = [];
+    for (let j = this.height - 1; j >= 0; j--) {
+       let row = [];
+       for (let i = 0; i < this.width; i++) {
+
+          row.push({
+             val: Math.random() > 0.2 ? 1 : 0,
+             i: i,
+             j: j
+          }
+          );
+       }
+       body.push(row);
+    }
+    return body;
+ }
+}
+
+module.exports = GameField;
